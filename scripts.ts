3f82938a2b17a251bad6a709eb9d06ed6acd8ed9
Rishabh <rishabhbhatia57@gmail.com>
2022-07-29 15:42:37 +0530

    Undo Changes

--- scripts.ts
+++ scripts.ts
@@ -80,53 +80,6 @@ let myTheme = EditorView.theme({
 
 
 
-
-// var PatientId = PatientInfo.PatientId
-// var MI1_Client_ID = PatientInfo.MI1ClientID
-// var encounterReference = PatientInfo.EncounterID
-// var practitionerReference = PatientInfo.ParctitionerID
-
-// console.log(PatientId)
-
-declare var FHIR
-
-var ret = $.Deferred();
-
-function onError() {
-	console.log('Loading error', arguments);
-	ret.reject();
-}
-
-function onReady(smart) {
-	if (smart.hasOwnProperty('patient')) {
-		var patient = smart.patient;
-		// console.log(patient);
-		console.log("Smart Object", smart);
-		var parctitionerId = smart.userId;
-		console.log('parctitionerId: ', parctitionerId);
-		var patientID = patient.id;
-
-		var encounterID = smart.tokenResponse.encounter;
-		localStorage.setItem('encounterRef', encounterID);
-		localStorage.setItem('fhirpatientid', patientID);
-		localStorage.setItem('parctitionerId', parctitionerId);
-
-
-		// $('#loading').hide();
-		var pt = patient.read();
-
-		for (var key in localStorage) {
-			console.log(key, localStorage.getItem(key))
-		}
-
-	} else {
-		onError();
-	}
-
-}
-
-FHIR.oauth2.ready(onReady, onError);
-
 let PatientId = localStorage.getItem('fhirpatientid')
 let MI1_Client_ID = localStorage.getItem('MI1ClientId')
 var encounterReference = localStorage.getItem('encounterRef')
